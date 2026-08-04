ce7448cb491639ca217d2d8910d2908c5d3534d6
Dmytro <dimaplyakin2007@gmail.com>
2026-08-04 22:00:38 +0300

    added MainLayout, routing, global css

---
+++ src/components/MainLayout/MainLayout.jsx
@@ -0,0 +1,24 @@
+// import "./MainLayout.module.scss";
+import { Outlet } from "react-router-dom";
+import styles from "./MainLayout.module.scss";
+
+export function MainLayout() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <div className={styles.mainLayout}>
+      <header>header</header>
+
+      <div className={styles.wrapper}>
+        <main className={styles.main}>
+          <Outlet />
+        </main>
+
+        <footer className={styles.footer}>
+          React Question Cards Application | {currentYear} <br />
+          by Dmytro
+        </footer>
+      </div>
+    </div>
+  );
+}
