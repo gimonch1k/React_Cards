025c09aef0db18245a6720d6720c95d5429a82ad
Dmytro <dimaplyakin2007@gmail.com>
2026-08-05 14:35:50 +0300

    added useEffect

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,33 @@
+import { useEffect, useState } from "react";
+
+import { API_URL } from "../../constants";
 import { QuestionCard } from "../../components/QuestionCard";
+import { Button } from "../../components/Button";
 
 import styles from "./HomePage.module.scss";
 
 export function HomePage() {
+  const [questions, setQuestions] = useState([]);
+
+  const getQuestions = async () => {
+    try {
+      const response = await fetch(`${API_URL}/react`);
+      return await response.json();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    getQuestions().then(setQuestions);
+  }, []);
+
   return (
     <>
       homepage
-      {/* <QuestionCard /> */}
+      {questions.map((card) => (
+        <QuestionCard card={card} key={card.id} />
+      ))}
     </>
   );
 }
